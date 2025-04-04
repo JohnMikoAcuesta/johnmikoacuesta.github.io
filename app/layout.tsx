beb3de43a7c23a07150f7febdf2881d88981bf05
John Miko Acuesta <acuesta.johnmiko@gmail.com>
2025-04-04 15:02:31 +0400

    Added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,20 @@ export default function RootLayout({
               gtag('config', 'G-39BLJXT9ZB');
             `,
           }}></script>
+
+<script async src="https://www.googletagmanager.com/ns.html?id=GTM-TWD8LW82" dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TWD8LW82');
+            `,
+          }}></script>
       </head>
       <body>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWD8LW82"
+      height="0" width="0"></iframe></noscript>
         <Navbar />
         <main className="relative overflow-hidden">
           {children}
